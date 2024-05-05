cc7d146d5d87de2fc79ec4a359b8c11e7ba8e101
Mohamed <mmuseaden@gmail.com>
2024-05-05 04:10:20 +0100

    requirements.txt added

--- staff.py
+++ staff.py
@@ -1,29 +1,28 @@
 from prettytable import PrettyTable
 
-
 class Staff:
     """Small staff blueprint, who will be able to refil the machine"""
     def __init__(self) -> None:
         self.employer = "employer"
         self.employee = "employee"
+        self.ingredients
     
     def refil_resources(self, resources_obj):
+            """Refils the machine"""
             table = PrettyTable()
-            table.align = "l"
-            resource_list = [ resources_obj.resources['water'], resources_obj.resources['milk'], resources_obj.resources['coffee'] ]
+
+            resource_list = [ resources_obj.resources["water"], resources_obj.resources["milk"], resources_obj.resources["coffee"] ]
             table.add_column("Ingredients", ["water", "milk", "coffee"])
             table.add_column("Amount", resource_list)
-            # for key, val in resources_obj.items():
-            #      print(key, val)
+            table.align = "l"
+            
             print("Welcome lets refil the machine.")
-            print(f"""\nCurrently we have:\n{table}""")
+            print(f"""Currently we have:\n{table}\n""")
             resources_obj.resources["water"] += int(input("Add Water. "))
             resources_obj.resources["milk"] += int(input("Add Milk. "))
             resources_obj.resources["coffee"] += int(input("Add Coffee. "))
-            # table.add_column("Amount", resource_list)
-            table.del_column("Amount")
-            resource_list = [ resources_obj.resources['water'], resources_obj.resources['milk'], resources_obj.resources['coffee'] ]
             
-
+            table.del_column("Amount")
+            resource_list = [ resources_obj.resources["water"], resources_obj.resources["milk"], resources_obj.resources["coffee"] ]            
             table.add_column("Amount", resource_list)
-            print(f"""\nThe new resources are:\n{table}""")
+            print(f"""\nThe new resources are:\n{table}\n""")
